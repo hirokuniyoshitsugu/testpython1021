9e3d62e080cdb75ba0ab6a5773d669535dc06e11
hirokuniyoshitsugu <hirokuni21531@yahoo.co.jp>
2020-10-20 19:04:45 +0900

    second

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-a = 10
-b = 3
+from browser import document
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a**b)
-print(a % b)
+
+def calc_bmi():
+    weight = float(document["weight"].value)
+    height = float(document["height"].value)
+
+    bmi = str(weight/(height*height))
+    rslt = document["result"]
+    rslt.text = bmi
+
+
+execute_btn = document["execute"]
+execute_btn.bind("click", calc_bmi)
